f26d79df8660a400d88d300a9e7ddea201cf058f
gewethor <gwethor@gmail.com>
2017-03-30 21:42:10 -0500

    Finished Tutorial

--- app/components/metawear-control.js
+++ app/components/metawear-control.js
@@ -3,7 +3,64 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	metawearConnected: false,
 	macAddressOfBoard: 'F3:30:D9:88:01:7E',
+	MWaccelHistory: [], 
+	updateAccelData: function(component, result){
+		component.set('x', result.x);
+		component.set('y', result.y);
+		component.set('z', result.z);
+
+		//update history, maintain 50 points max
+		var history=component.get('MWaccelHistory');
+		if(history.length === 150){
+			history.shiftObject();//shift an x off
+			history.shiftObject();//shift a y off
+			history.shiftObject();//shift a z off
+		}
+		var t = Date.now();
+		var newXPoint = {time: t, label: 'x', value: result.x};
+		var newYPoint = {time: t, label: 'y', value: result.y};
+		var newZPoint = {time: t, label: 'z', value: result.z};
+		history.addObjects([newXPoint, newYPoint, newZPoint]);
+		console.log('Added point: x=' + result.x + ', y='+result.y+', z='+result.z)
+	},
 	actions: {
+		accelON: function(){
+			var component = this;
+			Ember.run.later(function(){
+				wrapper to preserve binding satistfaction
+				try {
+				//invoke metawear connection
+					console.log('attempting to start accelerometer on: ' + component.get('macAddressOfBoard'));
+					metawear.mwdevice.startAccelerometer(
+						function(result){ //success
+							component.get('updateAccelData')(component,result);
+						}, function(error){//fail
+							console.log(error);
+							alert('error: '+error);
+						}
+					);
+				}
+				catch(err){
+					console.log('error: '+err);
+					alert('error: '+err);
+				}
+			}, 100);//run after 100ms
+		},
+		accelOFF: function(){
+			var component = this;
+			Ember.run.later(function(){
+				//wrapper to preserve binding satistfaction
+				try {
+				//invoke metawear connection
+					console.log('attempting to stop accelerometer on: ' + component.get('macAddressOfBoard'));
+					metawear.mwdevice.stopAccelerometer();
+				}
+				catch(err){
+					console.log('error: '+err);
+					alert('error: '+err);
+				}
+			}, 100);//run after 100ms
+		},
 		connect: function(){
 			var component = this;
 			Ember.run.later(function(){
